c1da84dee8772b88aa66b181021f4d1c76fc194c
Dmitry Zavodnikov <d.zavodnikov@gmail.com>
2017-10-02 19:04:21 +0500

    Update Version.js

--- Version.js
+++ Version.js
@@ -13,7 +13,8 @@ function Version() {
 
     var DEFAULT_VERSION = '1.0.0';
 
-    var blocks = [];
+    var self = this;
+    var versionFragments = [];
 
     var args = Array.from(arguments);
     if (args.length === 0) {
@@ -34,23 +35,51 @@ function Version() {
             throw 'Illegal argument ' + val;
         }
 
-        blocks.push(val);
+        versionFragments.push(val);
+    }
+
+    this.getVersionFragments = function() {
+        return versionFragments;
+    }
+
+    this.compare = function(version) {
+        var frags1 = self.getVersionFragments();
+        var frags2 = version.getVersionFragments();
+        var i;
+        for (i = 0; i < Math.min(frags1.length, frags2.length); ++i) {
+            intVal1 = parseInt(frags1[i]);
+            intVal2 = parseInt(frags2[i]);
+            if (isNaN(intVal1) || isNaN(intVal2)) {
+                return frags1[i].localeCompare(frags2[2]);
+            }
+            if (intVal1 === intVal2) {
+                continue;
+            } else {
+                if (intVal1 < intVal2) {
+                    return -1;
+                } else {
+                    return  1;
+                }
+            }
+        }
+        return 0;
     }
 
     this.nextVersion = function() {
         var i;
         var intVal;
-        for (i = blocks.length - 1; i >= 0; --i) {
-            intVal = parseInt(blocks[i]);
+        var frags = self.getVersionFragments();
+        for (i = frags.length - 1; i >= 0; --i) {
+            intVal = parseInt(frags[i]);
             if (!isNaN(intVal)) {
                 intVal = intVal + 1;
-                blocks[i] = intVal.toString();
+                frags[i] = intVal.toString();
                 break;
             }
         }
     }
 
     this.toString = function() {
-        return blocks.join('.');
+        return self.getVersionFragments().join('.');
     }
 }
